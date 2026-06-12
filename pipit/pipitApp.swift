ec949e0a9cb02b5636289644fb8cf17f2d60488c
Ho Trong Hien <115549171+hienhayho@users.noreply.github.com>
2026-06-12 15:56:47 +0700

    Initial Commit

---
+++ pipit/pipitApp.swift
@@ -0,0 +1,17 @@
+//
+//  pipitApp.swift
+//  pipit
+//
+//  Created by hienht on 12/6/26.
+//
+
+import SwiftUI
+
+@main
+struct pipitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pipitTests/pipitTests.swift
@@ -0,0 +1,17 @@
+//
+//  pipitTests.swift
+//  pipitTests
+//
+//  Created by hienht on 12/6/26.
+//
+
+import Testing
+@testable import pipit
+
+struct pipitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
